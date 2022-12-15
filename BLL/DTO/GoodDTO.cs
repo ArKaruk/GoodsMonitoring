3d83be22a212720042f4d777b82d7b4c0a45a837
Artemiy <apkaruk@gmail.com>
2022-12-16 01:30:37 +0200

    DTO added

---
+++ BLL/DTO/GoodDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class GoodDTO
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int StorageId { get; set; }
+
+        public int Amount { get; set; }
+        public int TypeId { get; set; }
+    }
+}
